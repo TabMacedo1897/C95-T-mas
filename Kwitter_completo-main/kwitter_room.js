7888b888b854823ba7adb195f028088a5391c87f
Professora Tábata Macedo <106038097+TabMacedo1897@users.noreply.github.com>
2023-03-31 18:10:08 -0300

    Add files via upload

---
+++ Kwitter_completo-main/kwitter_room.js
@@ -0,0 +1,14 @@
+//adicione o código do seu banco de dados
+const firebaseConfig = {
+  apiKey: "AIzaSyALAnJV1JezO-Bi1RiZAmuLcfCqtgwwppc",
+  authDomain: "kwitterbd-c9b27.firebaseapp.com",
+  databaseURL: "https://kwitterbd-c9b27-default-rtdb.firebaseio.com",
+  projectId: "kwitterbd-c9b27",
+  storageBucket: "kwitterbd-c9b27.appspot.com",
+  messagingSenderId: "765789229419",
+  appId: "1:765789229419:web:1f8efde36d26b887c4bc2e"
+};
+
+//// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+
